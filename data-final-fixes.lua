42123cf8a90cef251ad1ac15ab21f591b326e47e
Abu Dhabi <adhabi@gmail.com>
2025-04-16 22:31:38 +0200

    Refine raw materials list

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -53,25 +53,54 @@ for _, tech_with_prereqs in pairs(technology_tree) do
     end
 end
 
-local raw_materials = {}; -- string array
-local all_results = {}; -- string->bool dictionary
-local all_ingredients = {}; -- string->bool dictionary
+local all_raw_materials = {}; -- string->bool dictionary
+local advanced_raw_materials = {}; -- string->bool dictionary
+local results_in_technologies = {}; -- string->bool dictionary
+local ingredients_in_technologies = {}; -- string->bool dictionary
 for _, tech in pairs(technology_tree) do
     for result_name, _ in pairs(tech.results_unlocked) do
-        all_results[result_name] = true
+        results_in_technologies[result_name] = true
     end
     for ingredient_name, _ in pairs(tech.ingredients_used) do
-        all_ingredients[ingredient_name] = true
+        ingredients_in_technologies[ingredient_name] = true
+    end
+end
+for ingredient_name, _ in pairs(ingredients_in_technologies) do
+    if not results_in_technologies[ingredient_name] then
+        advanced_raw_materials[ingredient_name] = true
     end
 end
 
-for ingredient_name, _ in pairs(all_ingredients) do
-    if not all_results[ingredient_name] then
-        raw_materials[ingredient_name] = true
+local initial_raw_materials = {}; -- string->bool dictionary
+local initial_results = {}; -- string->bool dictionary
+local initial_ingredients = {}; -- string->bool dictionary
+for recipe_name, recipe in pairs(data.raw.recipe) do
+    if (recipe.enabled == nil or recipe.enabled == true) and (recipe.hidden ~= true) and (recipe.category ~= "parameters") then
+        util.logg(recipe_name)
+        if recipe.results then
+            for _, result in pairs(recipe.results) do
+                initial_results[result.name] = true
+            end
+        end
+        if recipe.ingredients then
+            for _, ingredient in pairs(recipe.ingredients) do
+                initial_ingredients[ingredient.name] = true
+            end
+        end
     end
 end
 
---util.logg(technology_tree)
-util.logg(all_results)
-util.logg(all_ingredients)
-util.logg(raw_materials)
+for ingredient_name, _ in pairs(initial_ingredients) do
+    if not initial_results[ingredient_name] then
+        initial_raw_materials[ingredient_name] = true
+    end
+end
+
+for key, _ in pairs(advanced_raw_materials) do
+    all_raw_materials[key] = true
+end
+for key, _ in pairs(initial_raw_materials) do
+    all_raw_materials[key] = true
+end
+
+util.logg(all_raw_materials)
