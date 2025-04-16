91b1153b5bb65c62fdb592cd6d13f6c043243986
AbuDhabi <adhabi@gmail.com>
2025-04-16 18:33:45 +0200

    Initial upload

---
+++ data-final-fixes.lua
@@ -0,0 +1,6 @@
+
+
+-- Get all technologies.
+-- Create an ancestor graph from them. The technology plus its ancestors.
+-- For each technology, randomize its recipe ingredients allowing stuff already unlocked in its ancestors and itself (except the item in question).
+
