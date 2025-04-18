80e213166b92ea83afaed2ce67405a07bf99f872
Abu Dhabi <adhabi@gmail.com>
2025-04-18 14:21:47 +0200

    Copy original information to raws, prepare barreling subgroup list

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -7,8 +7,18 @@ local ingredients = require "scripts.ingredients"
 -- For each technology, randomize its recipe ingredients allowing stuff already unlocked in its ancestors and itself (except the item in question).
 
 
+local ignored_recipe_subgroups = {
+    ["fill-barrel"] = true,
+    ["empty-barrel"] = true
+};
+
+-- Save a copy of each recipe's original version in the game data.
+for recipe_name, recipe_raw in pairs(data.raw.recipe) do
+    local copy = table.deepcopy(data.raw.recipe[recipe_name])
+    data.raw.recipe[recipe_name].original = copy
+end
 
 local technology_tree = tech.build_tech_tree();
 local initial_ingredients_available = ingredients.find_initially_available_ingredients(technology_tree);
 
-util.logg(initial_ingredients_available)
+util.logg(initial_ingredients_available)
