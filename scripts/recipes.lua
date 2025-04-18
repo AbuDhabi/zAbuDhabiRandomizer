d120f71322aaac39b0047fae96fc642d9e0134d3
Abu Dhabi <adhabi@gmail.com>
2025-04-18 19:12:08 +0200

    Add tentative algorithm for detecting more recipes, disabled for now

--- scripts/recipes.lua
+++ scripts/recipes.lua
@@ -1,25 +1,29 @@
 local utilities = require "scripts.utilities"
+local defines = require "scripts.defines"
 
 local F = {};
 
-function F.get_recipe_raw_materials(data_raw_recipes, recipe_name, amount_demanded)
-    local found_recipe = data_raw_recipes[recipe_name]
+function F.get_recipe_raw_materials(recipes, item_or_fluid_name, amount_demanded)
+    local found_recipe = recipes[item_or_fluid_name] -- Naively try a recipe with the exact same name.
+    if found_recipe == nil and false then -- TODO: Currently this finds coal-synthesis and breaks.
+        found_recipe = F.try_to_find_a_non_obvious_recipe(recipes, item_or_fluid_name)
+    end
     if found_recipe then
         local found_raw_materials = {};
-        if found_recipe.original.ingredients == nil then
+        if found_recipe.ingredients == nil then
             return {} -- No ingredients.
         end
-        for _, ingredient in pairs(found_recipe.original.ingredients) do
-            if recipe_name == ingredient.name then
-                return recipe_name -- Recipe is a breeder, ie. produces something from itself. Need to avoid loop.
+        for _, ingredient in pairs(found_recipe.ingredients) do
+            if item_or_fluid_name == ingredient.name then
+                return item_or_fluid_name -- Recipe is a breeder, ie. produces something from itself. Need to avoid loop.
             end
             local amount_produced = 1;
-            for _, result in pairs(found_recipe.original.results) do
+            for _, result in pairs(found_recipe.results) do
                 if result.name == found_recipe then
                     amount_produced = result.amount
                 end
             end
-            local ingredient_raw_materials = F.get_recipe_raw_materials(data_raw_recipes, ingredient.name, ingredient.amount)
+            local ingredient_raw_materials = F.get_recipe_raw_materials(recipes, ingredient.name, ingredient.amount)
             if type(ingredient_raw_materials) == "table" then
                 for raw_material_name, raw_material_amount in pairs(ingredient_raw_materials) do
                     local already_counted = found_raw_materials[raw_material_name] or 0
@@ -31,7 +35,7 @@ function F.get_recipe_raw_materials(data_raw_recipes, recipe_name, amount_demand
         end
         return found_raw_materials
     else
-        return recipe_name -- Doesn't have a recipe, it's a raw material.
+        return item_or_fluid_name -- Doesn't have a recipe, it's a raw material.
     end
 end
 
@@ -47,6 +51,21 @@ function F.filter_out_ignored_recipes(data_raw_recipes)
         end
     end
     return filtered_recipes
+end
+
+function F.try_to_find_a_non_obvious_recipe(recipes, item_or_fluid_name)
+    local amount_of_recipes_producing_this = 0
+    local production_recipe
+    for _, recipe in pairs(recipes) do
+        if recipe.results and #recipe.results == 1 and recipe.results[1].name == item_or_fluid_name then
+            amount_of_recipes_producing_this = amount_of_recipes_producing_this + 1
+            production_recipe = recipe
+        end
+    end
+    if amount_of_recipes_producing_this == 1 then
+        return production_recipe 
+    end
+    return nil
 end
 
 
