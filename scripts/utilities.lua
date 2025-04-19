62be08151b8e97f38e8ed77cceb30725dd607a95
Abu Dhabi <adhabi@gmail.com>
2025-04-19 22:42:48 +0200

    Add utility function to determine if a table's keys are a subset of another table's keys

--- scripts/utilities.lua
+++ scripts/utilities.lua
@@ -12,4 +12,18 @@ function F.merge_tables(table1, table2)
     return table1
 end
 
+function F.table_keys_subset(smaller_table, larger_table)
+    local smaller_table_length = 0;
+    for key, _ in pairs(smaller_table) do
+        smaller_table_length = smaller_table_length + 1
+    end
+    local keys_found = 0;
+    for key, _ in pairs(larger_table) do
+        if smaller_table[key] then
+            keys_found = keys_found + 1
+        end
+    end
+    return keys_found == smaller_table_length
+end
+
 return F
