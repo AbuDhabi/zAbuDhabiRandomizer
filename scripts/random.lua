3d1efff2d139a3b8f63e39dc0b70dae8ef3326e0
Abu Dhabi <adhabi@gmail.com>
2025-04-23 23:55:08 +0200

    Add documentation

--- scripts/random.lua
+++ scripts/random.lua
@@ -5,7 +5,7 @@ local D20, D40 = 1048576, 1099511627776  -- 2^20, 2^40
 local X1 = 0
 local X2 = 1
 
----@return number Floating point value between [0, 1]
+---@return number Result Floating point value between [0, 1]
 function F.value()
     local U = X2*A2
     local V = (X1*A2 + X2*A1) % D20
@@ -25,21 +25,21 @@ function F.seed(seed)
 end
 
 ---@param max number Maximum integer
----@return number Integer between [1, max]
+---@return number Result Integer between [1, max]
 function F.int(max)
     return math.floor(F.value()*max) + 1
 end
 
 ---@param min number Minimum integer
 ---@param max number Maximum integer
----@return number Integer between [min, max]
+---@return number Result Integer between [min, max]
 function F.range(min, max)
     return min + F.int(max - min + 1) - 1
 end
 
 ---@param min number Minimum float
 ---@param max number Maximum float
----@return number Floating point value between [min, max]
+---@return number Result Floating point value between [min, max]
 function F.float_range(min, max)
     return min + F.value() * (max - min)
 end
--- scripts/raw_materials.lua
+++ scripts/raw_materials.lua
@@ -1,5 +1,10 @@
 local F = {};
 
+---Calculates raw material "costs", ie. sums of fluid and item raw materials.
+---@param data_raw_item table Wube-provided data.raw.item
+---@param data_raw_fluid table Wube-provided data.raw.fluid
+---@param raw_materials table Keys: item names, values: amounts needed to produce one unit
+---@return table Scores { item: number, fluid: number }
 function F.get_raw_material_costs(data_raw_item, data_raw_fluid, raw_materials)
     local scores = { item = 0, fluid = 0};
     for material_name, material_amount in pairs(raw_materials) do
